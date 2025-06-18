34f4dc3a67cfc092e1a5edc14a9d0244cf4bf5ae
doniyor <usupovd276@gmail.com>
2025-06-18 19:12:46 +0300

    fix bugs

--- components/shared/owners.tsx
+++ components/shared/owners.tsx
@@ -288,8 +288,8 @@ export function OwnerTable() {
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
+      <div className="flex items-center space-x-2 py-4">
+        <div className="text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{' '}
           {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
